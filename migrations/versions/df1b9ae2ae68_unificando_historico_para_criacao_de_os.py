2a09c654d60603ac5fb65a29624ecf63c2279ce8
Pedro-Vilas-IJA <pedro.vilasboas@ijadrones.com.br>
2026-03-02 10:36:40 -0300

    feat: migração para o banco de dados

---
+++ migrations/versions/df1b9ae2ae68_unificando_historico_para_criacao_de_os.py
@@ -0,0 +1,24 @@
+"""unificando historico para criacao de OS
+
+Revision ID: df1b9ae2ae68
+Revises: 1600d07df8f3, 9922f618dc90
+Create Date: 2026-03-02 10:30:29.999761
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'df1b9ae2ae68'
+down_revision = ('1600d07df8f3', '9922f618dc90')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
